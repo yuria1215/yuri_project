799a76742a2e5b1a5edb678e2cb80f0443d481b5
Reila W <ravennreila@gmail.com>
2021-04-17 23:28:18 +0800

    0417

---
+++ 01.js
@@ -0,0 +1,11 @@
+/*
+js型態
+1. 字串 string                     ex: 'str',"str"
+2. 數字 number                     ex:   1
+3. 布林值  boolean                 ex:   true / false
+4. 物件 object                     ex:  { a : 1 , "name"屬性key : "yuri"屬性值value}
+5. 空值 不定義 undefined
+6. 空值(人工設定) null
+7. symbol
+*/
+
